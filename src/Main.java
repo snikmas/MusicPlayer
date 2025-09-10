6b01c8f9f006193516f74f6677b838cf03378f17
MarySnik <mary.snikk@gmail.com>
2025-09-10 20:42:13 +0800

    new

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import javax.swing.*;
+
+public class Main {
+
+    // instance of JFrame
+    JFrame frame = new JFrame();
+    MusicPlayer(){
+
+    }
+
+    public static void main(String[] args) {
+        // here we go again
+
+
+    }
+}
